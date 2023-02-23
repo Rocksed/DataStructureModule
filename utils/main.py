5e325d4efc40db63acbcc829ecc7e981ace3dc22
Rocksed <ilyagotin@mail.ru>
2023-02-23 17:36:06 +0300

    Initial commit

---
+++ utils/main.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        node = Node(data, self.top)
+        self.top = node
+
+
+n1 = Node(5, None)
+n2 = Node('a', n1)
+print(n1.data)
+print(n2.data)
+print(n1)
+print(n2.next_node)
+
+stack = Stack()
+stack.push('data1')
+stack.push('data2')
+stack.push('data3')
+print(stack.top.data)
+print(stack.top.next_node.data)
+print(stack.top.next_node.next_node.data)
+print(stack.top.next_node.next_node.next_node)
+print(stack.top.next_node.next_node.next_node.data)
