ea89f48a6a17373f754b02b9ddb1f52565b452e3
Rocksed <ilyagotin@mail.ru>
2023-03-02 09:37:46 +0300

    The pop() method of the Stack class is implemented. Updated tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,5 @@
 import unittest
-
-from utils.main import Node, Stack
+from utils.main import Stack
 
 
 class TestStack(unittest.TestCase):
@@ -8,17 +7,20 @@ class TestStack(unittest.TestCase):
         stack = Stack()
         stack.push('data1')
         self.assertEqual(stack.top.data, 'data1')
-        stack.push('data2')
-        self.assertEqual(stack.top.data, 'data2')
-        stack.push('data3')
-        self.assertEqual(stack.top.data, 'data3')
 
-    def test_node(self):
-        n1 = Node(5, None)
-        n2 = Node('a', n1)
-        self.assertEqual(n1.data, 5)
-        self.assertEqual(n2.data, 'a')
-        self.assertEqual(n2.next_node, n1)
+    def test_pop(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        data = stack.pop()
+        self.assertEqual(data, 'data2')
+        self.assertEqual(stack.top.data, 'data1')
+
+    def test_pop_empty_stack(self):
+        stack = Stack()
+        data = stack.pop()
+        self.assertIsNone(data)
+        self.assertIsNone(stack.top)
 
 
 if __name__ == '__main__':
--- utils/main.py
+++ utils/main.py
@@ -9,23 +9,40 @@ class Stack:
         self.top = None
 
     def push(self, data):
-        node = Node(data, self.top)
-        self.top = node
+        new_node = Node(data)
+        new_node.next_node = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            return None
+
+        data = self.top.data
+        self.top = self.top.next_node
+        return data
+
+stack = Stack()
+stack.push('data1')
+data = stack.pop()
+
+# stack is now empty
+print(stack.top)
 
 
-n1 = Node(5, None)
-n2 = Node('a', n1)
-print(n1.data)
-print(n2.data)
-print(n1)
-print(n2.next_node)
+# pop() remove item and return returned data
+print(data)
+
+
 
 stack = Stack()
 stack.push('data1')
 stack.push('data2')
-stack.push('data3')
+data = stack.pop()
+
+# now the last element contains data1
 print(stack.top.data)
-print(stack.top.next_node.data)
-print(stack.top.next_node.next_node.data)
-print(stack.top.next_node.next_node.next_node)
-print(stack.top.next_node.next_node.next_node.data)
+
+
+# data of the deleted item
+print(data)
+
