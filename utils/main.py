62dfee54f053ffbbdb691764ca27f9d5b82d8fcd
Rocksed <ilyagotin@mail.ru>
2023-03-03 09:00:15 +0300

    Magical methods

--- utils/main.py
+++ utils/main.py
@@ -1,3 +1,6 @@
+from custom_queue import Queue
+
+
 class Node:
     def __init__(self, data, next_node=None):
         self.data = data
@@ -21,6 +24,7 @@ class Stack:
         self.top = self.top.next_node
         return data
 
+
 stack = Stack()
 stack.push('data1')
 data = stack.pop()
@@ -28,11 +32,8 @@ data = stack.pop()
 # stack is now empty
 print(stack.top)
 
-
 # pop() remove item and return returned data
 print(data)
-
-
 
 stack = Stack()
 stack.push('data1')
@@ -42,7 +43,16 @@ data = stack.pop()
 # now the last element contains data1
 print(stack.top.data)
 
-
 # data of the deleted item
 print(data)
 
+queue = Queue()
+queue.enqueue('data1')
+queue.enqueue('data2')
+queue.enqueue('data3')
+
+print(queue.head.data)  
+print(queue.head.next_node.data)
+print(queue.tail.data)
+print(queue.tail.next_node)
+print(queue.tail.next_node.data)
